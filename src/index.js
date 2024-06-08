0f11b599ab09ddbb88471e68ea01b014a9e925b5
willtechcod <dev.pereira2019@gmail.com>
2024-06-08 00:32:22 -0300

    feat: add container initial page in home

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./style/global.css";
+import Home from './pages/home/home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-  <h1>Cardápio Digital</h1>
+  <>
+    <Home />
+  </>
 );
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,23 @@
+import Navbar from "../../components/navbar/navbar";
+
+const Home = () => {
+  return ( 
+    <>
+      <Navbar/>
+
+      <div className="container">
+        <div className="titulo">
+            <h1>Nosso Cardápio</h1>
+              <p>
+                  Clique em adicionar para colocar os produtos na sacola  de compras.
+                  Se preferir, você pode pedir pelo whatsApp: (47) 99999-9999<br/>
+            </p>
+        </div>
+      </div>
+      
+                  lista dos produtos...
+    </>
+   );
+}
+ 
+export default Home;
