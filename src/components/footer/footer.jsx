69c9b41ab6f076062ed31026961dcf4398500667
willtechcod <dev.pereira2019@gmail.com>
2024-06-09 12:29:28 -0300

    feat: create components footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+const Footer = () => {
+  return ( 
+  <div className="footer">
+    <p className="text-center">&copy; todos os direitos reservados Good-Food, desenvolvido por <span>WILLTECHCOD.COM</span></p>
+  </div> 
+  );
+}
+ 
+export default Footer;
