df412c0fbf18d998b9637fc86354dc3f7892652c
willtechcod <dev.pereira2019@gmail.com>
2024-06-09 12:34:02 -0300

    chore: add new configuration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./style/global.css";
-import Home from './pages/home/home';
+import Rotas from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <Home />
+    <Rotas />
   </>
 );
